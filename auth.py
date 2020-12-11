81720f3a25a3c44126565fd9e0f212825cd30686
mohamedrazzk <mohamedrazzk@gmail.com>
2020-12-12 01:43:28 +0200

    auth update
    auth class now support login with mail or username 
    also support reset using username or email registered in database 
    reset function will send email with code to user 
    code_validation the next step for validating code 
    reset handler last used class to change user password on database

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-
 """
 Project :SWE-CSE2020
 Created by : Razzk
@@ -8,7 +7,6 @@ Last-M : Razzk
 M-date 12/8/2020 01:25pm
 """
 
-
 from data_rw import user
 from tinydb import TinyDB, Query
 from sechashuli import make_pw_hash, check_pw_hash
@@ -17,9 +15,9 @@ from mreset import mail_code
 
 db = TinyDB('usrdb.json')
 users = Query()
+ccdenum=0
 
-
-class user_auth:                    #user login authintication
+class user_auth:  # user login authintication
 
     def __init__(self, username, password):
         self.username = username
@@ -27,35 +25,61 @@ class user_auth:                    #user login authintication
 
     def login(self):
         error = ""
-        status=False
-        data = db.get(self.username == Query()['username'])
+        status = False
+        if "@" in self.username:
+            data = db.get(self.username == Query()['email'])
+        else:
+            data = db.get(self.username == Query()['username'])
         if data == None:
-            error = "User Not Found"
+            error = "User Or Emal  Not Found"
+            return status, error
         elif check_pw_hash(self.password, data.get('password')):
-            error=("Right Password")
+            error = ("Right Password")
             status = True
         else:
             error = "Wrong Password"
 
-        return status, error                                                 #that funcation return error casue and status
+        return status, error  # that funcation return error casue and status
+
 
     def reset(self):
-        data = db.get(self.username == Query()['username'])
-        mail = data.get('email')
-        reset_code = randint(100000, 999999)
-        mail_code(mail, reset_code)
-        mail_reset_code = int(input())
+        if "@" in self.username:
+            if db.get(self.username == Query()['email']) is not None:
+                mail = db.get(self.username == Query()['email']).get('email')
+            else:
+                return False , "Mail Not Found"
 
-        if mail_reset_code == reset_code:
-            new_password=str(input("please Enter new Password\n"))
-            db.update({"password": make_pw_hash(new_password)}, users.username == self.username)
-            print("password had changed ")
         else:
-            print("worng mail Code")
+            if db.get(self.username == Query()['username']) is not None:
+                mail = db.get(self.username == Query()['username']).get('email')
+            else:
+                return False, "User Not Found"
 
+        global ccdenum
+        ccdenum = randint(100000, 999999)
+        mail_code(mail, ccdenum)
+        return "Code had been Sent"
+
+
+    def code_validaion(self,code):
+        mail_reset_code = int(code)
+        if mail_reset_code == ccdenum:
+            return True
+        else:
+            return "Wrong Reset Code "
+
+    def reset_handler(self,password):
+        new_password = str(password)
+        db.update({"password": make_pw_hash(new_password)}, users.username == self.username)
+        return True
 
 
 """
+print(user_auth("mohamedrazzk","").reset())
+x=input()
+print(user_auth("","").code_validaion(x))
+print(user_auth("mohamedrazzk",None).reset_handler("pass@word"))
+
 #print(user_auth("mohamedrazzk", "pass@word").login())
 user_auth("mohamedrazzk","").reset()                             //give function just user name 
 
