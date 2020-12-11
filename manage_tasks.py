cf1dcaefef286c348e3307635432f65905f9a1f3
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2020-12-11 08:56:46 +0000

    Add files via upload
    My first attempt on the class that manages the tasks, will be optimizing the code later on.
    Note: currently the sort_by_end_date method causes an issue which I am working on solving :)

---
+++ manage_tasks.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+"""
+Project:SWE-CSE2020
+Created by: Dina Ashraf
+C-Date: 12/10/2020 
+"""
+
+
+# In[346]:
+
+
+from datetime import datetime 
+#import datetime as dt
+from tinydb import TinyDB, Query 
+from operator import itemgetter
+db = TinyDB('usrdb.json')                           
+users = Query()  
+
+db_tasks=TinyDB('taskdb.json')
+tasks=Query()
+
+
+# In[399]:
+
+
+class Tasks():
+    """Class that handles all functions related to tasks"""
+    def __init__(self):
+        #A bunch of empty lists that will contain details about tasks
+        self.tasks=[]
+        self.scores=[]
+        self.deadlines=[]
+        self.places=[]
+        self.partners=[]
+        self.start_dates=[]
+        self.status = "On-going" #Status: state of the task: finished or on-going
+        
+    def add_task(self, user, task, score, deadline, place="", partner=""):
+        """Method that creates the tasks and adds them to lists"""
+        self.user=user
+        self.task=task
+        self.score=score
+        self.deadline=deadline  #Please define the deadline in the following format: %d/%m/%Y %H:%M:%S
+        self.place=place
+        self.partner=partner
+        
+        self.now = datetime.now()
+        self.start_date= self.now.strftime("%d/%m/%Y %H:%M:%S")
+        
+        self.tasks.append(self.task)
+        self.scores.append(self.score)
+        self.deadlines.append(self.deadline)
+        self.partners.append(self.partner)
+        self.start_dates.append(self.start_date)
+        self.places.append(self.place)
+        
+        db_tasks.insert({"username": self.user, "task":self.task, "score":self.score ,
+                         "start_date": self.start_date, "deadline": self.deadline,
+                         "place": self.place, "partners": self.partner, "status" : self.status})
+     
+        db.update({'tasks': self.tasks, 'task_start_date':self.start_dates, 'score':self.scores, 'deadline':self.deadlines, 
+                   'partner':self.partners, 'place':self.places}, self.user== users.username )
+        
+    def show_tasks(self):
+        for task in self.tasks:
+            print(task)
+        
+    def sort_by_name(self):
+        print("Tasks ordered alphabetically by name: ")
+        for task in sorted(self.tasks):
+            print(task)
+            
+    def sort_by_end_date(self):
+        
+        tasks.deadline =datetime.strptime(str(tasks.deadline), '%d/%m/%y %H:%M')
+        self.undecorated = db_tasks.search(tasks.username==self.user) #All tasks associated with the user
+        self.result = sorted(self.undecorated, key=itemgetter('deadline'))
+        for i in self.result:
+            print(i['task'] , i['deadline'])
+        
+            
+    def mark_as_finished(self,name):
+        self.finished_task_name = name
+        db_tasks.update({"status" : "Finished" } , self.finished_task_name== tasks.task)    
+    def show_tasks_in_progress(self):
+        self.on_going_tasks = db_tasks.search(tasks.status == "On-going")
+        for i in self.on_going_tasks:
+            print(i['task'])
+            
+
+
+# In[175]:
+
+
+#Create a user for testing
+db.insert(
+                    {'first_name': 'Dina', 'last_name': 'Ashraf', 'email': 'ddd1111@gmail.com',
+                     'phone': 111111111,
+                     'username': 'dinaashraf',
+                     'password': 10, 'role': 'user'})
+
+
+# In[400]:
+
+
+#Create an instance of the list of tasks
+to_do_list =  Tasks()
+#Add tasks
+to_do_list.add_task('dinaashraf', 'Do homework', 10, '15/10/2019 01:02', place="Cairo") #task with place attribute and no partner
+to_do_list.add_task('dinaashraf', 'Write code', 10, '12/12/2020 01:01') #task with neither place nor partner attribute
+
+
+# In[401]:
+
+
+to_do_list.add_task('dinaashraf', 'Write the code of thr project', 10, '11/10/2020 02:01' , "cairo", "mohamedrazzk") #task with partner and place attributes
+
+
+# In[402]:
+
+
+to_do_list.sort_by_name()
+
+
+# In[403]:
+
+
+#Testing the sort_by_end_date()
+to_do_list.sort_by_end_date()
+
+
+# In[329]:
+
+
+#Testing mark_as_finished()
+to_do_list.mark_as_finished("Do homework")
+
+
+# In[330]:
+
+
+#Testing showing tasks in progress
+to_do_list.show_tasks_in_progress()
+
+
+# In[354]:
+
+
+db_tasks.truncate() #-- clearing out the tasks database
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
