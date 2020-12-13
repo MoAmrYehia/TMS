bc8516669b1a806e22b3631a4a81c284bb25df82
Ehab Ebrahem <40669600+ehabebrahim@users.noreply.github.com>
2020-12-13 21:11:17 +0200

    Update login_mechanism.py

--- login_mechanism.py
+++ login_mechanism.py
@@ -1,7 +1,10 @@
-# -*- coding: utf-8 -*-
 """
-login mechanism 
-made by ehab ebrahim 
+Project :SWE-CSE2020
+Created by : ehab
+C-Date : 12/12/2020 , 7:15Pm
+Des: main gui for login
+Last-M : ehab
+M-date 12/12/2020 12:00pm
 """
 
 # signals use connect
@@ -31,27 +34,24 @@ class login(QMainWindow):
         self.stay_in.toggled.connect(self.gotostayin)
         
     def loginfunc(self):
-        email=self.email.text()
-        password=self.pasS.text()
-        print(email,password)
-        use=user_auth(email,password)
-        print(use.login())
+        status,error=user_auth(self.email.text(),self.pasS.text()).login()
+        print(error)
+        """ if status == True then go to the main windwo of the app"""
+                    
+       
         
         
     def gotosignup(self):
-        signUp=signup()
-        widget.addWidget(signUp)
+        widget.addWidget(signup())
         widget.setCurrentIndex(widget.currentIndex()+1)
         
         
     def gotoadmin(self):
-        Admin=admin()
-        widget.addWidget(Admin)
+        widget.addWidget(admin())
         widget.setCurrentIndex(widget.currentIndex()+1)
         
     def gotoforget(self):
-        Forget=forgetpass()
-        widget.addWidget(Forget)
+        widget.addWidget(forgetpass())
         widget.setCurrentIndex(widget.currentIndex()+1)
         
     def gotostayin(self):
@@ -68,9 +68,10 @@ class admin(QMainWindow):
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
     
     def loginfunc(self):
-        email=self.email.text()
-        password=self.password.text()
-        print(email,password)
+        status,error=user_auth(self.email.text(),self.password.text()).login()
+        print(error)
+        
+        """if status == True then go to the admin windwo"""
         
         
         
@@ -83,11 +84,11 @@ class forgetpass(QMainWindow):
         self.sendmail.clicked.connect(self.sendEmail)
         
     def sendEmail(self):
-        #check validation email syntax
-        
-        Email=self.email.text()
-        print("send email to",Email)
-        lo =writecode()
+          
+        user_auth(self.email.text(),"").reset()
+        # want a function take the email only and send the code
+        print("send email to",self.email.text())
+        lo =writecode(self.email.text())
         widget.addWidget(lo)
         widget.setCurrentIndex(widget.currentIndex()+1)
         
@@ -97,32 +98,34 @@ class forgetpass(QMainWindow):
     
     
 class writecode(QMainWindow):
-    def __init__(self):
+    def __init__(self,email):
         super(writecode,self).__init__()
         loadUi("write_code.ui",self)
+        self.email=email
         self.code.setPlaceholderText("Enter code")
         self.code.setEchoMode(QtWidgets.QLineEdit.Password)
         self.send.clicked.connect(self.gotosend)
         
     def gotosend(self):
         #make checking code
+        status=user_auth(str(self.email),"").code_validaion(self.code.text())
         
         #if ok goto change pass
-        make=makepass()
-        widget.addWidget(make)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-        #else 
-        #forget=forgetpass()
-       # widget.addWidget(forget)
-       # widget.setCurrentIndex(widget.currentIndex()+1)
+        if status==True:
+            widget.addWidget(makepass(self.email))
+            widget.setCurrentIndex(widget.currentIndex()+1)
+        else:
+            widget.addWidget(forgetpass())
+            widget.setCurrentIndex(widget.currentIndex()+1)
         
         
         
         
 class makepass(QMainWindow):
-    def __init__(self):
+    def __init__(self,email):
         super(makepass,self).__init__()
         loadUi("changepass.ui",self)
+        self.email=email
         self.passw.setPlaceholderText("password")
         self.confpass.setPlaceholderText("confirm password")
         self.passw.setEchoMode(QtWidgets.QLineEdit.Password)
@@ -131,11 +134,15 @@ class makepass(QMainWindow):
     
     def gotoreset(self):
         if self.passw.text()==self.confpass.text():
-            password=self.passw.text()
-            print("password changed",password)
-            logiin=login()
-            widget.addWidget(logiin)
-            widget.setCurrentIndex(widget.currentIndex()+1)
+            status=user_auth(str(self.email),"").reset_handler(self.passw.text())
+            if status==True:
+                widget.addWidget(login())
+                widget.setCurrentIndex(widget.currentIndex()+1)
+            else:
+                print("enter another pass")
+            
+        else:
+            print("notmatched")
     
     
     
@@ -151,7 +158,7 @@ class signup(QMainWindow):
         self.email.setPlaceholderText("Email")
         self.username.setPlaceholderText("username")
         self.phone.setPlaceholderText("phone number")
-        self.role.setPlaceholderText("role")
+        #self.role.setPlaceholderText("role")
         self.password.setPlaceholderText("password")
         self.conf.setPlaceholderText("confirm password")
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
@@ -159,23 +166,15 @@ class signup(QMainWindow):
         
         
     def creatfunc(self):
-        first_Name=self.firstname.text()
-        second_Name=self.lastname.text()
-        email=self.email.text()
-        username=self.username.text()
-        phone=self.phone.text()
-        role=self.role.text()
-        password=self.password.text()
-        conf=self.conf.text()
-        if password==conf:
+        if self.password.text()==self.conf.text():
             print("matched")
-            User=user(first_Name,second_Name,email,phone,username,role,password)
-            if User.adduser() == True:
-                logiin=login()
-                widget.addWidget(logiin)
+            User=user(self.firstname.text(),self.lastname.text(),self.email.text(),self.phone.text(),self.username.text(),self.role.currentText(),self.password.text())
+            status,error=User.adduser()
+            if status==True:
+                widget.addWidget(login())
                 widget.setCurrentIndex(widget.currentIndex()+1)
             else:
-                print("in label error check your data")
+                print(error)
                 
         else:
             print("Not matched")
@@ -187,68 +186,14 @@ class signup(QMainWindow):
 
 
 app=QApplication(sys.argv)
-mainwindow=login()
 widget=QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedWidth(600)
-widget.setFixedHeight(620)
+widget.addWidget(login())
+#widget.setGeometry()
+#widget.setWindowState(WindowMaximized)
+#widget.showFullScreen()
+#widget.setFixedWidth(1024)
+#widget.setFixedHeight(800)
 widget.show()
 app.exec_()
 
         
-
-
-# class MyWindow(QMainWindow):
-#     def __init__(self):
-#         super(MyWindow, self).__init__()
-#         #win=QMainWindow()
-#         self.setGeometry(1000,1000,800,600)
-#         self.setWindowTitle("first")
-#         self.initUi()
-        
-#     def initUi(self):
-        
-#         self.b=QtWidgets.QPushButton(self)
-#         self.b.setText("Admin")
-#         self.b.move(20,20)
-         
-#         self.b1=QtWidgets.QPushButton(self)
-#         self.b1.setText("User")
-#         self.b1.move(120,20)
-         
-        
-#         self.label=QtWidgets.QLabel(self)
-#         self.label.setText("email")
-#         self.label.move(50,50)
-#         self.line=QtWidgets.QLineEdit(self)
-#         self.line.move(100,50)
-       
-#         self.label1=QtWidgets.QLabel(self)
-#         self.label1.setText("password")
-#         self.label1.move(50,100)
-#         self.line1=QtWidgets.QLineEdit(self)
-#         self.line1.move(100,100)
-        
-#     def 
-        
-#         # self.b1=QtWidgets.QPushButton(self)
-#         # self.b1.setText("Click me")
-#         # self.b1.clicked.connect(self.clicked)
-        
-#     def clicked_admin(self):
-#         self.label.setText("preesed")
-# s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #TCP connection
-
-# def window():
-#     app=QApplication(sys.argv)
-#     win=MyWindow()
-#    # xpos=0,ypos=0,width=5,hight=5
-#     #win.setGeometry(1020,1020,300,300)
-#     #win.setWindowTitle("first")
-#     # label=QtWidgets.QLabel(win)
-#     # label.setText("ehab dfddf")
-#     # label.move(50,50)
-#     win.show()
-#     sys.exit(app.exec_())
-    
-# window()
