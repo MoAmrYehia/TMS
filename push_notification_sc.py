e62ef7e37fdaa2ea28ffb2daa4e82e38d69d139f
mohamedrazzk <mohamedrazzk@gmail.com>
2020-12-20 14:36:18 +0200

    Windows support
    now this notification function support both windows and linux notification center

--- push_notification_sc.py
+++ push_notification_sc.py
@@ -1,12 +1,20 @@
-import notify2
-notify2.init('Task Manager')
+
+import platform
 
 def push_notification(message):
+    if platform.system() == "Windows":
+        from win10toast import ToastNotifier
+        toast = ToastNotifier()
+        toast.show_toast("SWE CSE Task Manager", message, duration=20) #ico can be added later
+    else:
+        import notify2
+        notify2.init('Task Manager')
+        n = notify2.Notification("SWE CSE Task Manager ",
+                                 message,
+                                 "notification-message-im"  # Icon name
+                                 )
+        n.show()
 
-    n = notify2.Notification("SWE CSE Task Manager ",
-                             message,
-                             "notification-message-im"   # Icon name
-                            )
-    n.show()
 
-push_notification("You have task now ")
+
+#push_notification("You have task now ")
