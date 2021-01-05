3c2cf3e146a4cfeb4707a483e0bda4fdd87f02cb
Dina Abdul-Rasoul <73671062+dinaabdulrasoul@users.noreply.github.com>
2021-01-05 20:29:32 +0200

    Update push_notification_sc.py

--- push_notification_sc.py
+++ push_notification_sc.py
@@ -5,12 +5,12 @@ def push_notification(message):
     if platform.system() == "Windows":
         from win10toast import ToastNotifier
         toast = ToastNotifier()
-        toast.show_toast("SWE CSE Task Manager", message, duration=20) #ico can be added later
+        toast.show_toast("Task Manager ", message, duration=20) #ico can be added later
 
     else:
         import notify2
         notify2.init('Task Manager')
-        n = notify2.Notification("SWE CSE Task Manager ",
+        n = notify2.Notification("Task Manager ",
                                  message,
                                  "notification-message-im"  # Icon name
                                  )
