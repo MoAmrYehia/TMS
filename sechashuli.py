cd7c95edc59b6ee61b824d017e173486b264a0b4
RaZzk <mohamedrazzk@gmail.com>
2021-01-10 05:42:34 +0200

    Code Cleaning|Reformatting code & delete redundant codes

--- sechashuli.py
+++ sechashuli.py
@@ -9,16 +9,12 @@ M-date 12/7/2020 10:50pm
 
 import hashlib
 
+
 def make_pw_hash(passwod):
-    return hashlib.sha256(str.encode(passwod)).hexdigest()        #returning hashed password for validating
+    return hashlib.sha256(str.encode(passwod)).hexdigest()  # returning hashed password for validating
 
 
-def check_pw_hash(password,hash):                                 #hashchiching funcatio
-     if make_pw_hash(password) == hash:
-         return True
-     return False
-
-
-
-#print(make_pw_hash("pass@word"))
-#print(check_pw_hash("pass@word","a0f4f5e99ec8ca18b26b2f9c64d35194ee497a95b907614aadb2c90b93d88626"))
+def check_pw_hash(password, hash):  # hashing function
+    if make_pw_hash(password) == hash:
+        return True
+    return False
